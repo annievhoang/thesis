5b9b59704611e8686f1222b8f92f51f046d8b013
annievhoang <anniehoang267@gmail.com>
2023-10-26 22:22:14 -0400

    add p5

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let questionText = "ask the Internet";
+let x = 40, y = 40;
+let n = 3;
+let numSentences = 3;
+
+let markov;
+let source;
+let displayText = [""];
+
+function preload() {
+  source = loadStrings('source.txt');
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+  background(0, 0, 0);
+  fill(0);
+  textSize(18)
+  let inp = createInput(questionText);
+  inp.position(50, 450);
+  inp.size(200);
+  button = createButton('submit');
+  button.position(250, 450);
+  button.mousePressed(generateText);
+  // create a markov model w' n=4
+  markov = RiTa.markov(n);
+  // load text into the model
+  markov.addText(source.join(' '));
+  drawText();
+}
+
+function drawText() {
+  background(0, 82, 0);
+  text(displayText.join(' '), x, y, 800, 440);
+  textSize(30);
+  fill(255, 255, 255);
+}
+
+function generateText() {
+  displayText = markov.generate(numSentences);
+  drawText();
+}
